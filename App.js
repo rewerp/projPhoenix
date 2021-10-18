4ae86ab1291736383ea77b43316b1af766aaf74b
Rewer <rewer_p@hotmail.com>
2021-10-18 17:52:34 -0300

    commit-Navegacao-16-10-2021-17-52

--- App.js
+++ App.js
@@ -1,21 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+function HomeScreen({navigation}) {
+   return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+         <Text>Home Screen</Text>
+         <Button title="Navegar" onPress={() => navigation.navigate('DetailsScreen')} />
+      </View>
+   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function DetailsScreen() {
+   return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+         <Text>Details Screen</Text>
+      </View>
+   );
+}
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+   return (
+      <NavigationContainer>
+         <Stack.Navigator initialRouteName="Home" >
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="DetailsScreen" component={DetailsScreen} />
+         </Stack.Navigator>
+      </NavigationContainer>
+   );
+}
