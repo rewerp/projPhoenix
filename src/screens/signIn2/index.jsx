d83ee31b61cf3cc72bc62fbd75dbaef03b97c5e4
Rewer <rewer_p@hotmail.com>
2021-10-20 18:00:08 -0300

    commit-20-10-2021-18-00
    Ajustes de codigo

--- src/screens/signIn2/index.jsx
+++ src/screens/signIn2/index.jsx
@@ -1,7 +1,9 @@
 import React, { setState } from 'react';
-import { View, ScrollView, Text, Image, 
-         StatusBar, TextInput, SafeAreaView,
-         TouchableOpacity, Linking } from 'react-native';
+import {
+   View, ScrollView, Text, Image,
+   StatusBar, TextInput, SafeAreaView,
+   TouchableOpacity, Linking
+} from 'react-native';
 
 import { styles } from './styles';
 import { theme } from '../../global/theme';
@@ -9,57 +11,55 @@ import { ButtonDefault } from '../../components/buttonDefault';
 
 import LogoApp from '../../assets/logoPhoenix.png';
 import LogoDobes from '../../assets/logoDobes.png';
- 
-function SignIn2({navigation}) {
-   return(
-      //  <SafeAreaView style={styles.safeArea}  >
-          <ScrollView style={styles.scrollView} contentContainerStyle={{flexGrow: 1}} >
-            <View style={styles.mainView} >
-               <View style={styles.topImgView} >
-                  <StatusBar 
-                     barStyle="light-content"
-                     backgroundColor="transparent"
-                     translucent   
+
+function SignIn2({ navigation }) {
+   return (
+      <ScrollView style={styles.scrollView} contentContainerStyle={{ flexGrow: 1 }} >
+         <View style={styles.mainView} >
+            <View style={styles.topImgView} >
+               <StatusBar
+                  barStyle="light-content"
+                  backgroundColor="transparent"
+                  translucent
+               />
+               <Image
+                  style={styles.logoApp}
+                  source={LogoApp}
+               />
+            </View>
+            <View style={styles.componentsView} >
+               <View style={styles.contentForm} >
+                  <TextInput
+                     placeholder="CNPJ"
+                     placeholderTextColor={theme.colors.placeholderTextColor}
+                     style={styles.textInput}
+                     keyboardType="number-pad"
                   />
-                  <Image 
-                     style={styles.logoApp}
-                     source={LogoApp} 
+                  <TextInput
+                     placeholder="Senha"
+                     placeholderTextColor={theme.colors.placeholderTextColor}
+                     style={styles.textInput}
+                     secureTextEntry
                   />
-               </View>
-               <View style={styles.componentsView} >
-                  <View style={styles.contentForm} >
-                     <TextInput 
-                        placeholder="CNPJ" 
-                        placeholderTextColor={theme.colors.placeholderTextColor}
-                        style={styles.textInput} 
-                        keyboardType="number-pad"
-                     />
-                     <TextInput 
-                        placeholder="Senha" 
-                        placeholderTextColor={theme.colors.placeholderTextColor}
-                        style={styles.textInput}
-                        secureTextEntry
-                     />
-                     <Text 
-                        style={styles.textPress} 
-                        onPress={() => navigation.navigate('RecoveryPassword')} 
-                     >
-                        Esqueci a minha senha
-                     </Text>
-                     <ButtonDefault style={styles.buttonSignIn} title="Acessar" />
-                  </View>
-               </View>
-               <View style={styles.bottomImgView} >
-                  <TouchableOpacity onPress={() => Linking.openURL('https://dobesone.com.br/')} >
-                     <Image 
-                        style={styles.logoDobes} 
-                        source={LogoDobes} 
-                     />
-                  </TouchableOpacity>
+                  <Text
+                     style={styles.textPress}
+                     onPress={() => navigation.navigate('RecoveryPassword')}
+                  >
+                     Esqueci a minha senha
+                  </Text>
+                  <ButtonDefault style={styles.buttonSignIn} title="Acessar" />
                </View>
             </View>
-          </ScrollView>
-      //  </SafeAreaView>
+            <View style={styles.bottomImgView} >
+               <TouchableOpacity onPress={() => Linking.openURL('https://dobesone.com.br/')} >
+                  <Image
+                     style={styles.logoDobes}
+                     source={LogoDobes}
+                  />
+               </TouchableOpacity>
+            </View>
+         </View>
+      </ScrollView>
    )
 }
 
